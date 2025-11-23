384176eba668123e496ed0047a6134df5f99dd3e
Seung-Won <chawind122@ajou.ac.kr>
2025-11-23 18:16:49 +0900

    Update Topic Paraphrasing and Retrieval-based Positive Sampling.py

--- ProtoXTM/Stage2/Topic Paraphrasing and Retrieval-based Positive Sampling.py
+++ ProtoXTM/Stage2/Topic Paraphrasing and Retrieval-based Positive Sampling.py
@@ -6,7 +6,6 @@ from rank_bm25 import BM25Okapi
 from collections import defaultdict, Counter
 from gensim.models import KeyedVectors
 
-
 def read_text(path):
     texts = list()
     with open(path, 'r', encoding='utf-8', errors='ignore') as file:
@@ -38,8 +37,8 @@ ft_cn = fasttext.load_model('/workspace/fasttext/cc.zh.300.bin')
 
 
 # vocab
-vocab_en_path = "/users/seung-won/documents/datasets/Amazon_Review/AR_vocab_en"
-vocab_cn_path = "/users/seung-won/documents/datasets/Amazon_Review/AR_vocab_cn"
+vocab_en_path = "/workspace/Amazon_Review/AR_vocab_en"
+vocab_cn_path = "/workspace/Amazon_Review/AR_vocab_cn"
 
 # raw texts
 train_texts_en_path = "/workspace/Amazon_Review/train_texts_en.txt"
@@ -65,7 +64,6 @@ topic_cn_trans = clean_chinese_topic_words(topic_cn_trans)
 
 
 def paraphrasing(topic_en_trans, vocab_en, ft_en, threshold):
-
     processed_topics = []
     
     for topic in topic_en_trans:
@@ -93,9 +91,8 @@ def paraphrasing(topic_en_trans, vocab_en, ft_en, threshold):
                 continue
         
         processed_topics.append(" ".join(processed_words))
-    
     return processed_topics
-
+    
 
 def paraphrasing_cn(topic_cn_trans, vocab_cn, ft_cn, threshold):
 
@@ -124,12 +121,8 @@ def paraphrasing_cn(topic_cn_trans, vocab_cn, ft_cn, threshold):
                     processed_words.append(best_match)
             else:
                 continue
-        
-        
         processed_topics.append(" ".join(processed_words))
-    
     return processed_topics
-
 
 
 paraphrasing_topics_en = paraphrasing(topic_en_trans, vocab_en, ft_en, threshold=0.4)
@@ -137,13 +130,9 @@ paraphrasing_topics_cn = paraphrasing_cn(topic_cn_trans, vocab_cn, ft_cn, thresh
 
 
 def get_top_documents_for_topics(topics, train_texts_en, top_n):
-
     tokenized_documents = [doc.split() for doc in train_texts_en]
-    
     bm25 = BM25Okapi(tokenized_documents)
-   
     topic_results = {}
-
     for topic_idx, topic_words in enumerate(topics):
      
         doc_scores = defaultdict(float)
@@ -155,12 +144,10 @@ def get_top_documents_for_topics(topics, train_texts_en, top_n):
                
                 doc_scores[doc_idx] += score
 
-        
         top_docs = sorted(
             doc_scores.items(), key=lambda x: x[1], reverse=True
         )[:top_n]
 
-        
         topic_results[f"Topic_{topic_idx + 1}"] = [
             {
                 "doc_index": idx,
@@ -179,10 +166,8 @@ top_documents_cn_200 = get_top_documents_for_topics(paraphrasing_topics_cn, trai
 
 # Pseudo Labeling
 from collections import defaultdict
-
 def pseudo_label_documents(top_documents, total_docs):
 
-    
     doc_label_data = defaultdict(lambda: {"topic_index": -1, "rank": float("inf"), "score": -1})
     
     for topic_idx, docs in enumerate(top_documents.values()):
@@ -204,14 +189,11 @@ def pseudo_label_documents(top_documents, total_docs):
 
     return labels
 
-
 total_docs_en = 20000
 labels_c2e_200 = pseudo_label_documents(top_documents_en_200, total_docs_en)
 
 total_docs_cn = 20000  
 labels_e2c_200 = pseudo_label_documents(top_documents_cn_200, total_docs_cn)
-
-
 
 def save_labels_to_npy(labels, file_path):
 
