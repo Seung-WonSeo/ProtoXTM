2258105b43700526d4f1985452784a42ca399ddd
Seung-Won <chawind122@ajou.ac.kr>
2025-11-23 20:12:25 +0900

    Update run.py

--- Trainer/run.py
+++ Trainer/run.py
@@ -1,33 +1,32 @@
-# 사용 예제 on ECNews
 if __name__ == "__main__":
-    # SBERT 문서 임베딩 및 BoW 표현 로드
-    doc_embeddings_en_path = "/users/seung-won/documents/datasets/Amazon_Review/AR_sbert_doc_embeddings_en.npz"
-    doc_embeddings_cn_path = "/users/seung-won/documents/datasets/Amazon_Review/AR_sbert_doc_embeddings_cn.npz"
+    # Multi-lingual BERT embeddings
+    doc_embeddings_en_path = "/workspace/Amazon_Review/AR_sbert_doc_embeddings_en.npz"
+    doc_embeddings_cn_path = "/workspace/Amazon_Review/AR_sbert_doc_embeddings_cn.npz"
     
-    # doc_embeddings_en_path = "/users/seung-won/documents/datasets/Amazon_Review/AR_sbert_doc_embeddings_en.npz"
-    # doc_embeddings_cn_path = "/users/seung-won/documents/datasets/Amazon_Review/AR_sbert_doc_embeddings_cn.npz"
+    # doc_embeddings_en_path = "/workspace/Amazon_Review/AR_sbert_doc_embeddings_en.npz"
+    # doc_embeddings_cn_path = "/workspace/Amazon_Review/AR_sbert_doc_embeddings_cn.npz"
     
-    # bow_embeddings_en_path = "/users/seung-won/documents/datasets/ECNews/ECN_bow_embeddings_en.npy"
-    # bow_embeddings_cn_path = "/users/seung-won/documents/datasets/ECNews/ECN_bow_embeddings_cn.npy"
+    # bow_embeddings_en_path = "/workspace/ECNews/ECN_bow_embeddings_en.npy"
+    # bow_embeddings_cn_path = "/workspace/ECNews/ECN_bow_embeddings_cn.npy"
     
-    bow_embeddings_en_path = "/users/seung-won/documents/datasets/Amazon_Review/AR_bow_embeddings_en.npy"
-    bow_embeddings_cn_path = "/users/seung-won/documents/datasets/Amazon_Review/AR_bow_embeddings_cn.npy"
+    bow_embeddings_en_path = "/workspace/Amazon_Review/AR_bow_embeddings_en.npy"
+    bow_embeddings_cn_path = "/workspace/Amazon_Review/AR_bow_embeddings_cn.npy"
 
-    labels_en_path = "/users/seung-won/documents/TPL_method/data/Amazon_Review/XLM_labels_en_50.npy"
-    labels_cn_path = "/users/seung-won/documents/TPL_method/data/Amazon_Review/XLM_labels_cn_50.npy"
+    labels_en_path = "/workspace/Amazon_Review/XLM_labels_en_50.npy"
+    labels_cn_path = "/workspace/Amazon_Review/XLM_labels_cn_50.npy"
     
-    # labels_en_path = "/users/seung-won/documents/TPL_method/data/Amazon_Review/k=50/labels_en.npy"
-    # labels_cn_path = "/users/seung-won/documents/TPL_method/data/Amazon_Review/k=50/labels_cn.npy"
+    # labels_en_path = "/workspace/Amazon_Review/k=50/labels_en.npy"
+    # labels_cn_path = "/workspace/Amazon_Review/k=50/labels_cn.npy"
 
     
-    # labels_c2e_path = "/users/seung-won/documents/RPS/data/ECNews/labels_c2e_70.npy"
-    # labels_e2c_path = "/users/seung-won/documents/RPS/data/ECNews/labels_e2c_70.npy"
+    # labels_c2e_path = "/workspace/ECNews/labels_c2e_70.npy"
+    # labels_e2c_path = "/workspace/ECNews/labels_e2c_70.npy"
     
-    labels_c2e_path = "/users/seung-won/documents/RPS/data/Amazon_Review/XLM_labels_c2e_30.npy"
-    labels_e2c_path = "/users/seung-won/documents/RPS/data/Amazon_Review/XLM_labels_e2c_30.npy"
+    labels_c2e_path = "/workspace/Amazon_Review/XLM_labels_c2e_30.npy"
+    labels_e2c_path = "/workspace/Amazon_Review/XLM_labels_e2c_30.npy"
 
-    sbert_doc_embeddings_en = np.load(doc_embeddings_en_path)  # English SBERT embeddings
-    sbert_doc_embeddings_cn = np.load(doc_embeddings_cn_path)  # Chinese SBERT embeddings
+    sbert_doc_embeddings_en = np.load(doc_embeddings_en_path)  # English document embeddings
+    sbert_doc_embeddings_cn = np.load(doc_embeddings_cn_path)  # Chinese document embeddings
     
     sbert_doc_embeddings_en = sbert_doc_embeddings_en['embeddings']
     sbert_doc_embeddings_cn = sbert_doc_embeddings_cn['embeddings']
@@ -42,7 +41,6 @@ if __name__ == "__main__":
     labels_e2c = np.load(labels_e2c_path)
     
 
-    # 모델 초기화
     input_size = sbert_doc_embeddings_en.shape[1]
     vocab_size_en = bow_en.shape[1]
     vocab_size_cn = bow_cn.shape[1]
@@ -50,14 +48,14 @@ if __name__ == "__main__":
     DCL_weight = 1
     temperature = 0.1
     
-    model = RPS_XTM(input_size=input_size, vocab_size_en=vocab_size_en, vocab_size_cn=vocab_size_cn,
-                    num_topics=num_topics, DCL_weight=DCL_weight,
+    model = ProtoXTM(input_size=input_size, vocab_size_en=vocab_size_en, vocab_size_cn=vocab_size_cn,
+                    num_topics=num_topics, DPCL_weight=DPCL_weight,
                     temperature=temperature, en_units=200, dropout=0.1)
 
-    # Optimizer 정의
+    # Optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
-    # DataLoader 생성
+    # DataLoader
     batch_size_en = 512
     batch_size_cn = 512
     
@@ -66,5 +64,5 @@ if __name__ == "__main__":
                                                               labels_cn, labels_e2c,
                                                               batch_size_en, batch_size_cn)
 
-    # 모델 학습
-    trained_model = train_RPSXTM(model, dataloader_en, dataloader_cn, optimizer, num_epochs=500, device='cpu')
+    # train
+    trained_model = train_ProtoXTM(model, dataloader_en, dataloader_cn, optimizer, num_epochs=500, device='cuda')
